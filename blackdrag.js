dc067d0b6f39161f29031ab693dffe3ecb864d72
Christophe Gragnic <christophegragnic@gmail.com>
2013-01-09 15:45:11 +0100

    Working.

--- blackdrag.js
+++ blackdrag.js
@@ -1,19 +1,19 @@
+// http://jsfiddle.net/wfbY8/4/
 function blackdrag_init(){
-    document.getElementById('blackdrag').addEventListener('mousedown', mouseDown, false);
+    var div = document.getElementById('blackdrag');
+    div.style.position = 'absolute';
+    div.style.bottom = 0;
+    div.addEventListener('mousedown', mouseDown, false);
     window.addEventListener('mouseup', mouseUp, false);
 }
-
 function mouseUp()
 {
     window.removeEventListener('mousemove', divMove, true);
 }
-
 function mouseDown(e){
     window.addEventListener('mousemove', divMove, true);
 }
-
 function divMove(e){
     var div = document.getElementById('blackdrag');
-    div.style.position = 'absolute';
     div.style.top = e.clientY + 'px';
 }
